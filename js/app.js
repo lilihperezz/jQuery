5e7d08c107e74abe6b6390e4a70f104c01408b29
lili hualtibamba <lilihperezz@gmail.com>
2016-10-11 16:24:22 -0500

    version 3

--- js/app.js
+++ js/app.js
@@ -1,17 +1,15 @@
-var enlaces = document.querySelectorAll("a");
-
-var cargaPagina = function () {
-	// var enlaces = document.getElementsByTagName("a");
-	//for (var i = 0, l = enlaces.length; i < l; i++) {
-	enlaces.forEach(function(enlace) {
-		// var enlace = enlaces[i];
-		var linkVideo = enlace.getAttribute("href");
-		var linkImagen = youtube.generateThumbnailUrl(linkVideo);
-		var img = document.createElement("img");
-		img.setAttribute("src", linkImagen);
-		enlace.appendChild(img);
-	});
-	//}
+var cargar = function() {
+	var $enlaces = $("a");
+	$enlaces.each(iterar);
 };
 
-window.addEventListener("load", cargaPagina);
+var iterar = function(index, element) {
+	var $enlace = $(element);
+	var linkVideo = $enlace.attr("href");
+	var linkImagen = youtube.generateThumbnailUrl(linkVideo);
+	var $img = $("<img>");
+	$img.attr("src", linkImagen);
+	$enlace.append($img);
+};
+
+$(document).ready(cargar);
